54a711ef511add3d845a3df25ec1f8b7f1cd9439
Benjamin Sherman <benjamin@holyarmy.org>
2023-09-07 18:25:24 -0500

    fix: provision act data dir

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+# use ucore path provisioning to ensure the act runner data dir exists
+echo "0755;/var/act_data" >> /etc/systemd/ucore-paths-provision.conf
+
 systemctl enable podman.socket
 systemctl enable podman-docker-socket.service
 

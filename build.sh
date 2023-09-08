794e95cc10916847d7299856c871ef7cb2d080f4
Benjamin Sherman <benjamin@holyarmy.org>
2023-09-07 19:35:36 -0500

    fix: add /var/act_data to paths again

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 # use ucore path provisioning to ensure the act runner data dir exists
+echo >> /etc/systemd/ucore-paths-provision.conf
 echo "0755;/var/act_data" >> /etc/systemd/ucore-paths-provision.conf
 
 systemctl enable podman.socket
